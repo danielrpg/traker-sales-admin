46f89c1db28b8f5219d3d1923f057629ec879c95
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-25 07:28:38 -0400

    version actualizada el sabado a la media noche

--- app/App.js
+++ app/App.js
@@ -1,5 +1,27 @@
 TukTuk.Modal.loading();
 setTimeout( TukTuk.Modal.hide, 1000);
-var App=(function(tuk, $, undefined){
-	console.log(tuk);
-})(TukTuk,$);
+var url = "http://localhost:3000/";
+ var map;
+$(document).on('ready', App(TukTuk));
+function App(tuk, undefined){
+	var socket = io.connect(url);
+	//console.log(socket);
+	socket.on('compartirPoscion',function(data){
+		console.dir(data);
+		map = new GMaps({
+		   div: '#id_mapa_geo',
+		   lat: data.lat,
+		   lng: data.lon,
+		   zoom: 16
+		 });
+		 contexto = '<div ><strong>'+data.login+'</strong></div> ';
+		 map.addMarker({
+		    lat: data.lat,
+		    lng: data.lon,
+			title: 'TRAKER RESPONSE',
+			infoWindow: {
+				content: contexto 
+			}
+		});
+	});
+};
