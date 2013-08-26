407572dc4c7143cf767ed42e1f5456ce7337fd81
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-26 11:06:16 -0400

    Actalizado con google maps

--- app/App.js
+++ app/App.js
@@ -1,20 +1,20 @@
 TukTuk.Modal.loading();
 setTimeout( TukTuk.Modal.hide, 1000);
 var url = "http://localhost:3000/";
- var map;
+var map;
 $(document).on('ready', App(TukTuk));
 function App(tuk, undefined){
 	var socket = io.connect(url);
 	//console.log(socket);
 	socket.on('compartirPoscion',function(data){
-		console.dir(data);
+		//console.dir(data);
 		map = new GMaps({
 		   div: '#id_mapa_geo',
 		   lat: data.lat,
 		   lng: data.lon,
 		   zoom: 16
 		 });
-		 contexto = '<div ><strong>'+data.login+'</strong></div> ';
+		 contexto = '<div><strong>'+data.login+'</strong></div> ';
 		 map.addMarker({
 		    lat: data.lat,
 		    lng: data.lon,
